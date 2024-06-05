f574562294221654f42014538ff872512828d010
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-06-05 11:43:47 -0400

    tasks are being added

--- src/test/java/com/cydeo/tests/week1/morning/Task01.java
+++ src/test/java/com/cydeo/tests/week1/morning/Task01.java
@@ -1,8 +1,35 @@
 package com.cydeo.tests.week1.morning;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.*;
+
+import java.util.*;
+
 public class Task01 {
 
     public static void main(String[] args) {
+
+        //  1- Open a firefox browser
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+
+
+        //  2- Goto https://vytrack.com/
+        driver.get("https://vytrack.com/");  // first way
+        //driver.navigate().to("https://vytrack.com/"); // second way
+
+
+        //3- verify login is displayed
+        // locate element
+        WebElement loginLink = driver.findElement(By.linkText("LOGIN"));
+        if (loginLink.isDisplayed()){
+            System.out.println("Test is passed.");
+        }else{
+            System.out.println("Test is failed.");
+        }
+
+
 
     }
 }
