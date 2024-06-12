35e3d2893091743a6d4516f0700e2d035ad0c866
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2024-06-12 12:24:33 -0400

    tasks are being added

---
+++ src/test/java/com/cydeo/tests/week2/morning/TC003.java
@@ -0,0 +1,38 @@
+package com.cydeo.tests.week2.morning;
+
+import com.cydeo.tests.utilities.*;
+import org.openqa.selenium.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+public class TC003 {
+
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = Driver.getDriver();
+    }
+
+    @Test
+    public void test1(){
+        driver.get("https://practice.cydeo.com/");
+
+
+        driver.findElement(By.linkText("Radio Buttons")).click();
+
+
+        Assert.assertTrue(driver.getTitle().contains("Radio buttons"));
+
+        WebElement redButton = driver.findElement(By.id("red"));
+        redButton.click();
+        Assert.assertTrue(redButton.isSelected());
+
+
+        WebElement footballButton = driver.findElement(By.id("football"));
+        footballButton.click();
+        Assert.assertTrue(footballButton.isSelected());
+
+    }
+}
